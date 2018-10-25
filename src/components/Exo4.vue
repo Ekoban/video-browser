68495c7bfb2aac531b94a4049b4af13eb04af6b9
Ekoban <Ekoban@users.noreply.github.com>
2018-10-25 18:22:23 +0200

    Adding last exo of section

---
+++ src/components/Exo4.vue
@@ -0,0 +1,36 @@
+const vm = new Vue({
+    el: '#root',
+    data() {
+        return {
+            buttonHidden: false,
+            listHidden: true,
+        };
+    },
+    methods: {
+        hideButton() {
+            console.log("hide the button")
+        },
+        hideList() {
+            console.log("hide the list")
+        },
+    },
+    computed: {
+        buttonStatus() {},
+        listStatus() {},
+    },
+
+    template: `
+      <div>
+        <a class='dropdown-button btn' :style="" href='#'>Open</a>
+        
+        <div>
+            <ul class='dropdown-content' @click="hideList">
+              <li><a href="#!">one</a></li>
+              <li><a href="#!">two</a></li>
+              <li class="divider"></li>
+              <li><a href="#!">three</a></li>
+            </ul>
+        </div>
+      </div>
+    `
+});
